dbc2ab1d99aa622b02e68e05e1b9877855fde5a0
IceCodeNew <32576256+IceCodeNew@users.noreply.github.com>
2024-10-21 03:29:05 +0800

    feat: config (powered by koanf)

--- go.mod
+++ go.mod
@@ -2,10 +2,23 @@ module github.com/IceCodeNew/telesend
 
 go 1.23.2
 
-require github.com/stretchr/testify v1.9.0
+require (
+	github.com/knadh/koanf/parsers/json v0.1.0
+	github.com/knadh/koanf/providers/file v1.1.2
+	github.com/knadh/koanf/v2 v2.1.1
+	github.com/samber/lo v1.47.0
+	github.com/stretchr/testify v1.9.0
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
+	github.com/knadh/koanf/maps v0.1.1 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/app/config/readConfig.go
@@ -0,0 +1,69 @@
+package config
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/IceCodeNew/telesend/pkg/fsHelper"
+	"github.com/IceCodeNew/telesend/pkg/httpHelper"
+	"github.com/knadh/koanf/parsers/json"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/v2"
+	"github.com/samber/lo"
+)
+
+var (
+	k        = koanf.New(".")
+	TSConfig *TelesendConfig
+)
+
+type TelesendConfig struct {
+	// bot token is supposed to be set in the environment variable
+	BotToken string
+	DBPath   string `koanf:"db_path"`
+	Verbose  bool   `koanf:"verbose"`
+}
+
+func (tsConfig *TelesendConfig) sanityCheck() error {
+	req, err := http.NewRequest(http.MethodGet,
+		fmt.Sprintf("https://api.telegram.org/bot%s/getMe", tsConfig.BotToken), nil)
+	var resp *http.Response
+
+	_, _, err = lo.AttemptWhileWithDelay(3, time.Second*10,
+		func(int, time.Duration) (error, bool) {
+			// BE AWARE as the resp is NOT GUARANTEED to be non-nil
+			resp, err = httpHelper.HttpReqHelper(req, tsConfig.Verbose)
+			if err != nil {
+				return err, true
+			}
+			return nil, false
+		})
+	if err != nil {
+		return fmt.Errorf("FATAL: the bot token failed check after 3 attempts, the last error was:\n %v", err)
+	}
+	defer resp.Body.Close()
+
+	return fsHelper.CreateDir(tsConfig.DBPath)
+}
+
+func ReadConfig() error {
+	telesendConfPath := "telesend.json"
+	if _path, found := os.LookupEnv("TELESEND_CONF_PATH"); found {
+		if regularFile, err := fsHelper.IsRegularFile(_path); err != nil {
+			return err
+		} else if regularFile {
+			telesendConfPath = _path
+		}
+	}
+
+	// default configuration MUST exist
+	if err := k.Load(file.Provider(telesendConfPath), json.Parser()); err != nil {
+		return err
+	}
+
+	k.Unmarshal("", &TSConfig)
+	TSConfig.BotToken = os.Getenv("TELESEND_BotToken")
+	return TSConfig.sanityCheck()
+}

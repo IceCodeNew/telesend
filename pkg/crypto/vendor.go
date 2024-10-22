54f38dfee9d16f373ddb928939d4fdd5b53dd389
IceCodeNew <32576256+IceCodeNew@users.noreply.github.com>
2024-10-22 21:41:50 +0800

    feat: RandAsciiBytes(): Avoid ambiguous characters
    like I, O, l, o (num 0 & 1 are still in the list)

--- pkg/crypto/vendor.go
+++ pkg/crypto/vendor.go
@@ -6,12 +6,16 @@ import (
 
 // Imported from https://stackoverflow.com/a/44477359/13631331, with modifications
 
-// len(encodeURL) == 64. This allows (x <= 256) x % 64 to have an even
-// distribution.
-const encodeURL = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_"
+// len(encodeStr) = 8+5+11+11+2+11+10+6 = 64.
+//
+// This allows (0 <= x <= 255) x % 64 to have an even distribution.
+const encodeStr = "ABCDEFGH" + "JKLMN" + "PQRSTUVWXYZ" +
+	"abcdefghijk" + "mn" + "pqrstuvwxyz" + "0123456789" +
+	"-_+=,."
 
-// A helper function create and fill a slice of length n with characters from
-// a-zA-Z0-9_-. It panics if there are any problems getting random bytes.
+// A helper function create and fill a slice of length n with characters from the following string:
+//
+// "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz0123456789-_+=,."
 func RandAsciiBytes(n int) ([]byte, error) {
 	// We will take n bytes, one byte for each character of output.
 	output, randomness := make([]byte, n), make([]byte, n)
@@ -22,10 +26,11 @@ func RandAsciiBytes(n int) ([]byte, error) {
 		return nil, err
 	}
 
-	for i := range output {
-		randomPos := uint8(randomness[i]) % uint8(len(encodeURL))
-		output[i] = encodeURL[randomPos]
+	for i, _lenEncodeStr := 0, uint8(len(encodeStr)); i < n; i++ {
+		randomPos := uint8(randomness[i]) % _lenEncodeStr
+		output[i] = encodeStr[randomPos]
 	}
+	randomness = nil
 
 	return output, nil
 }

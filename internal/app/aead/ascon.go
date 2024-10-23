4af0e6b1bbc620090b4ca1090ade7943f771ee5e
IceCodeNew <32576256+IceCodeNew@users.noreply.github.com>
2024-10-24 01:11:25 +0800

    rewrite: make the salt "salter"

--- internal/app/aead/ascon.go
+++ internal/app/aead/ascon.go
@@ -2,6 +2,9 @@ package aead
 
 import (
 	"fmt"
+	"math/rand/v2"
+	"strconv"
+	"strings"
 
 	"github.com/IceCodeNew/telesend/internal/app/config"
 	"github.com/IceCodeNew/telesend/pkg/crypto"
@@ -10,11 +13,12 @@ import (
 
 var (
 	additionalData     = []byte("IceCode/TELESEND")
+	block              *ascon.Cipher
 	fakeKey, fakeNonce []byte
 )
 var (
 	asconCipherNotInitErr = fmt.Errorf("ERROR: [Internal] Ascon cipher not initialized")
-	block                 *ascon.Cipher
+	invalidBotTokenErr    = fmt.Errorf("ERROR: [Internal] Invalid bot token format")
 )
 
 // TeleSend Use the specified telegram bot token to encrypt/decrypt the Bark Sender info
@@ -40,44 +44,55 @@ func DecAscon128a(ciphertext []byte) ([]byte, error) {
 	return plaintext, err
 }
 
-func deriveKeyAndNonce() error {
-	var (
-		botToken   = config.TSConfig.BotToken
-		passphrase []byte
-	)
-	for i := 1; i < len(botToken); i++ {
-		if botToken[i-1] == ':' {
-			passphrase = []byte(botToken[i:])
-			break
-		}
+func InitCipher() error {
+	_seed, passphrase, found := strings.Cut(config.TSConfig.BotToken, ":")
+	if !found {
+		return invalidBotTokenErr
 	}
-	if passphrase == nil {
-		return fmt.Errorf("ERROR: [User Input] Invalid bot token")
+	r := rand.NewChaCha8(genFixedSeed(_seed))
+
+	var err error
+	fakeKey, fakeNonce = deriveKeyAndNonce(r, []byte(passphrase))
+	if block, err = ascon.New(fakeKey, ascon.Ascon128a); err != nil {
+		return err
 	}
-
-	// if lenPassphrase is multiples of ascon.NonceSize
-	fakeSalt := additionalData
-
-	lenPassphrase, _allDigitsOne := len(passphrase), ascon.NonceSize-1
-	if lenPassphrase&_allDigitsOne != 0 {
-		padLen := (lenPassphrase + _allDigitsOne) &^ _allDigitsOne
-		padLen -= lenPassphrase
-		fakeSalt = additionalData[:padLen]
-	}
-
-	kn := crypto.DeriveKey(passphrase, fakeSalt, ascon.KeySize+ascon.NonceSize)
-	fakeKey, fakeNonce = kn[:ascon.KeySize], kn[ascon.KeySize:]
 	return nil
 }
 
-func init() {
-	err := deriveKeyAndNonce()
+func deriveKeyAndNonce(r *rand.ChaCha8, passphrase []byte) (key, nonce []byte) {
+	salt := make([]byte, 8)
+	r.Read(salt)
+
+	kn := crypto.DeriveKey(passphrase, salt, ascon.KeySize+ascon.NonceSize)
+	key, nonce = kn[:ascon.KeySize], kn[ascon.KeySize:]
+	return
+}
+
+const seed2 = uint64(198964)
+
+func genFixedSeed(s string) [32]byte {
+	seed1, err := strconv.ParseUint(s, 10, 64)
 	if err != nil {
 		panic(err)
 	}
 
-	block, err = ascon.New(fakeKey, ascon.Ascon128a)
-	if err != nil {
-		panic(err)
+	// It does not matter if the product overflowed
+	//
+	// The final result of the product would length at least 16 digits, at most 20 digits.
+	// (18446744073709551615)
+	product := seed1 * seed2
+	for product < uint64(1000000000000000) {
+		product *= seed2
 	}
+	_strProduct := strconv.FormatUint(product, 10)
+
+	seed := append(make([]byte, 0, 32), _strProduct...)
+
+	padLen := (32 - len(_strProduct))
+	// let compiler aware of the fact that
+	// padLen <= len(additionalData)
+	padLen %= len(additionalData) + 1
+
+	seed = append(seed, additionalData[:padLen]...)
+	return [32]byte(seed)
 }

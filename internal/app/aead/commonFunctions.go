2ac5bf1d6d1a76a57237560d4f1337ab25d2e50e
IceCodeNew <32576256+IceCodeNew@users.noreply.github.com>
2024-10-25 16:42:17 +0800

    refine: PredictableSeed

--- internal/app/aead/commonFunctions.go
+++ internal/app/aead/commonFunctions.go
@@ -11,17 +11,18 @@ func DeriveKey(passphrase, salt []byte, keyLen uint32) (key []byte) {
 }
 
 func PredictableSeed(seed1, seed2 uint64) [32]byte {
-	if seed1 > seed2 {
-		seed1, seed2 = seed2, seed1
-	}
-
+	seed1, seed2 = enlargeSeed(seed1), enlargeSeed(seed2)
 	// It does not matter if the product overflowed
 	//
 	// The final result of the product would length at least 16 digits, at most 20 digits.
-	// (18446744073709551615)
+	// math.MaxUint64: 18446744073709551615
 	product := seed1 * seed2
-	for product < uint64(1000000000000000) {
+	for i := 0; product < uint64(1000000000000000); i++ {
 		product *= seed2
+		if product == 0 {
+			product = seed1 + seed2 - minimumSeed<<1
+			product = enlargeSeed(uint64(i) * product)
+		}
 	}
 	_strProduct := strconv.FormatUint(product, 10)
 
@@ -35,5 +36,17 @@ func PredictableSeed(seed1, seed2 uint64) [32]byte {
 	seed = append(seed, additionalData[:padLen]...)
 	return [32]byte(seed)
 }
+
+func enlargeSeed(seed uint64) uint64 {
+	if seed >= minimumSeed {
+		return seed
+	} else if seed < 2 {
+		return minimumSeed
+	}
+	seed *= seed
+	return enlargeSeed(seed)
+}
+
+const minimumSeed uint64 = 1<<20 - 1
 
 var additionalData = []byte("IceCode/TELESEND")
